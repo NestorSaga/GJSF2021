e715eed715e34937b081b2a0ec3180ea942a80f4
Nesaga <gunter499@gmail.com>
2021-05-08 22:10:57 +0200

    Fire particles

--- Assets/Scripts/Weapons/Flamethrower.cs
+++ Assets/Scripts/Weapons/Flamethrower.cs
@@ -7,6 +7,10 @@ public class Flamethrower : Weapon
 
 
     public List<Transform> flameSpawns;
+
+    public Transform flameAnchor;
+
+    public GameObject fireParticles;
     void Start()
     {
         Debug.Log("Soy un lanzallamas!");
@@ -25,6 +29,9 @@ public class Flamethrower : Weapon
         foreach (Transform tr in flameSpawns)
         {
             tr.GetComponent<BoxCollider>().enabled = true;
+            fireParticles = Instantiate(fireParticles, tr.transform.position,Quaternion.LookRotation(tr.transform.position - flameAnchor.transform.position, new Vector3(0,0,0)));
+            fireParticles.transform.parent = gameObject.transform;
+
         }
 
     }
