19b115fa882f1bdae3b61164e9a6098177ec68f8
2das <devcapel@gmail.com>
2021-05-08 03:09:24 +0200

    New Player Input Configurado y cosillas

---
+++ Assets/Scripts/Players/PlayerController.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public enum Direction {UP, DOWN, LEFT, RIGHT}
+public enum State {IDLE, MOVING, ATTACKING}
+public class PlayerController : MonoBehaviour
+{
+    // Movement
+    [Header("Movement Parameters")]
+    public float speed = 5;
+    public float jumpForce = 5;
+    bool isGrounded; // Variable de control interna, en algun momento la podriamos llegar a usar.
+    private Rigidbody rb;
+    State state = State.IDLE;
+    Direction direction = Direction.DOWN;
+
+    // Attack
+    [Header("Attack Parameters")]
+    public GameObject hitbox;
+    public float attackRange = 1;
+    public float attackDuration = 0.2f;
+    public float damage = 5;
+
+    // Gestión de inputs
+    Vector3 playerInput;
+    bool jumpInput, attackInput;
+
+    public void OnMove(InputAction.CallbackContext ctx) => playerInput = new Vector3(ctx.ReadValue<Vector2>().x, 0 , ctx.ReadValue<Vector2>().y);
+    public void OnJump(InputAction.CallbackContext ctx) => jumpInput = ctx.ReadValueAsButton();
+    public void OnAttack(InputAction.CallbackContext ctx) => attackInput = ctx.ReadValueAsButton();
+    public void OnPause(InputAction.CallbackContext ctx) => Debug.Log("Pause!"); // Quiza ni lo termine haciendo el player en si...
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
+
+    private void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        //Direction
+
+        if (playerInput.x < 0) direction = Direction.LEFT;
+        else if (playerInput.x > 0) direction = Direction.RIGHT;
+        else if (playerInput.z < 0) direction = Direction.DOWN;
+        else if (playerInput.z > 0) direction = Direction.UP;
+
+        //Movement
+        rb.MovePosition(transform.position + playerInput * Time.deltaTime * speed);
+
+        //Jump
+        if (Physics.CheckSphere(transform.position - transform.up * GetComponent<CapsuleCollider>().height/2, 0.1f, (1 << 8)))
+        {
+            // Definimos grounded
+            isGrounded = true;
+
+            // Si salta...
+            if (jumpInput)
+                rb.velocity = Vector3.up * jumpForce;
+        }
+
+        else isGrounded = false;
+
+        //Attack
+        if (attackInput)
+            StartCoroutine(AttackCoroutine(attackDuration));
+
+        // Reseteamos bools de input
+        jumpInput = false;
+        attackInput = false;
+    }
+
+    IEnumerator AttackCoroutine(float duration)
+    {
+        switch (direction)
+        {
+            case Direction.UP:
+                hitbox.transform.localPosition = transform.forward * attackRange;
+                break;
+
+            case Direction.DOWN:
+                hitbox.transform.localPosition = -transform.forward * attackRange;
+                break;
+
+            case Direction.LEFT:
+                hitbox.transform.localPosition = -transform.right * attackRange;
+                break;
+
+            case Direction.RIGHT:
+                hitbox.transform.localPosition = transform.right * attackRange;
+                break;
+        }
+
+        hitbox.SetActive(true);
+
+        while (duration > 0)
+        {
+            duration -= Time.deltaTime;
+            yield return null;
+        }
+        hitbox.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Players/Player_2_Controller.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player_2_Controller : MonoBehaviour
+{
+    [System.Serializable]
+    public class Weapons
+    {
+        public GameObject weapon_1;
+        public GameObject weapon_2;
+        public GameObject weapon_3;
+        public GameObject weapon_4;
+    }
+    public Weapons weapons = new Weapons();
+
+
+    // Gestión de inputs
+    Vector2 weaponRotation;
+    bool rotateClockwise, rotateCounterclockwise, weapon_1, weapon_2, weapon_3, weapon_4;
+
+    public void OnRotate_Clockwise(InputAction.CallbackContext ctx) => rotateClockwise = ctx.ReadValueAsButton();
+    public void OnRotate_Counterclockwise(InputAction.CallbackContext ctx) => rotateCounterclockwise = ctx.ReadValueAsButton();
+    public void OnRotateWeapon(InputAction.CallbackContext ctx) => weaponRotation = ctx.ReadValue<Vector2>();
+    public void OnWeapon_1(InputAction.CallbackContext ctx) => weapon_1 = ctx.ReadValueAsButton();
+    public void OnWeapon_2(InputAction.CallbackContext ctx) => weapon_2 = ctx.ReadValueAsButton();
+    public void OnWeapon_3(InputAction.CallbackContext ctx) => weapon_3 = ctx.ReadValueAsButton();
+    public void OnWeapon_4(InputAction.CallbackContext ctx) => weapon_4 = ctx.ReadValueAsButton();
+    public void OnPause(InputAction.CallbackContext ctx) => Debug.Log("Pause!"); // Quiza ni lo termine haciendo el player en si...
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (weapon_1)
+        {
+            Attack_Weapon_1();
+        }
+
+        if (weapon_2)
+        {
+            Attack_Weapon_2();
+        }
+
+        if (weapon_3)
+        {
+            Attack_Weapon_3();
+        }
+
+        if (weapon_4)
+        {
+            Attack_Weapon_4();
+        }
+
+        // Reseteamos bools de input
+        rotateClockwise = false;
+        rotateCounterclockwise = false;
+        weapon_1 = false;
+        weapon_2 = false;
+        weapon_3 = false;
+        weapon_4 = false;
+    }
+
+    public void RotateCharacter(float direction)
+    {
+
+    }
+
+    public void RotateWeapons(float direction)
+    {
+
+    }
+
+    public void Attack_Weapon_1()
+    {
+        var w = weapons.weapon_1.GetComponent(typeof(Weapon)) as Weapon;
+        w.Fire();
+    }
+
+    public void Attack_Weapon_2()
+    {
+        var w = weapons.weapon_2.GetComponent(typeof(Weapon)) as Weapon;
+        w.Fire();
+    }
+
+    public void Attack_Weapon_3()
+    {
+        var w = weapons.weapon_3.GetComponent(typeof(Weapon)) as Weapon;
+        w.Fire();
+    }
+
+    public void Attack_Weapon_4()
+    {
+        var w = weapons.weapon_4.GetComponent(typeof(Weapon)) as Weapon;
+        w.Fire();
+    }
+
+    public void TakeDamage(float quantity)
+    {
+
+    }
+
+    public void Die()
+    {
+
+    }
+}
